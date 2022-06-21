c20483c87e8da80bff5605d94bafccabbd80cd3a
papaya202206 <yomitorisenyou2018@gmail.com>
2022-06-21 11:35:37 +0900

    kaihatu

--- keisan.js
+++ keisan.js
@@ -1,133 +1,98 @@
-var questions = [];
 var num = 0;
-function loadKanji1JSON(){
-    const xhr = new XMLHttpRequest();
 
-    // リクエスト
-    xhr.open("GET", 'kanji1.json');
-    
-    //リクエスト送信
-    xhr.send();
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // jsonをオブジェクトに変更
-            const jsonObj = JSON.parse(xhr.responseText);      
-            questions = jsonObj.question;
-            for (let item of jsonObj.question) {        
-                console.log("context: " + item.context + " options: " + item.options + " answers: " + item.answers);        
-            }
-            setQuestion();
-        }  
-    }
+function init(){
+    makeSujiBut(document.getElementById('buttonlist'), document.getElementById('answer'));
 }
+
+function start(){
+    setQuestion();
+}
+
+function setValueById(id, value){
+    document.getElementById(id).value = value;
+}
+
+function setInnerHTMLById(id, html){
+    document.getElementById(id).innerHTML = html;
+}
+
+
 
 function reset(){
     document.getElementById('answer').className = "form-control bg-white";
     setQuestion();
 }
 
+var counts = 2;
+var numbers = [];
+function setRandamNumbers(){
+    for (var i=0; i<counts; i++){
+        numbers[i] = getRandamNumber();
+    }
+}
+
+var digit = 2;
+var fraction = 0;
+function getRandamNumber(){
+    var value = 0;
+    if (digit == 1){
+        value = Math.round10(Math.random() * 11);
+    }else if (digit == 2){
+        value = Math.floor(Math.random() * 101);
+    }else if (digit == 3){
+        value = Math.floor(Math.random() * 1001);
+    }else{
+        value = Math.floor(Math.random() * 101);
+    }
+
+    if(fraction == 0){
+        return value;
+    }else if(fraction == 1){
+        return value + Math.round10(Math.random(), -1);
+    }else if(fraction == 2){
+        return value + Math.round10(Math.random(), -2);
+    }else{
+        return value;
+    }
+}
+
+
+var operation = "+";
 function setQuestion() {
-    var question = questions[num];
+    setValueById('answer','');
 
-    document.getElementById('buttonlist').innerHTML = "";
-    document.getElementById('answer').value = "";
+    setInnerHTMLById('number', '問題' + (num + 1));
 
-    document.getElementById('number').innerHTML = "もんだい " + (num+1);
-    document.getElementById('context').innerHTML = question.context;
+    var context;
+    for(var i=0; i<counts; i++){
+        context += numbers[i];
+        if(i < counts - 1){
+            context += operation
+        }
+    }
+    setInnerHTMLById('context', context);
 
     var prog = (num/questions.length) * 100;
     document.getElementById('progress').style = "width: " + prog + "%";
     document.getElementById('progress').innerHTML = prog + "%";
 
-    if (question.prefx != "") {
-        document.getElementById('prefx').innerHTML = question.prefx;
-        document.getElementById('prefx').className = "input-group-text";
-    } else {
-        document.getElementById('prefx').innerHTML = question.prefx;
-        document.getElementById('prefx').className = "input-group-text d-none";
-    }
+    // if (question.prefx != "") {
+    //     document.getElementById('prefx').innerHTML = question.prefx;
+    //     document.getElementById('prefx').className = "input-group-text";
+    // } else {
+    //     document.getElementById('prefx').innerHTML = question.prefx;
+    //     document.getElementById('prefx').className = "input-group-text d-none";
+    // }
 
-    if (question.sufx != "") {
-        document.getElementById('sufx').innerHTML = question.sufx;
-        document.getElementById('sufx').className = "input-group-text";
-    } else {
-        document.getElementById('sufx').innerHTML = question.sufx;
-        document.getElementById('sufx').className = "input-group-text d-none";
-    }
+    // if (question.sufx != "") {
+    //     document.getElementById('sufx').innerHTML = question.sufx;
+    //     document.getElementById('sufx').className = "input-group-text";
+    // } else {
+    //     document.getElementById('sufx').innerHTML = question.sufx;
+    //     document.getElementById('sufx').className = "input-group-text d-none";
+    // }
 
-    document.getElementById('kanjiChar').innerHTML = question.kanji;
-
-    makeHiraganaBut();
-}
-
-const hiragana = [
-    'あ','い','う','え','お',
-    'か','き','く','け','こ',
-    'さ','し','す','せ','そ',
-    'た','ち','つ','て','と',
-    'な','に','ぬ','ね','の',
-    'は','ひ','ふ','へ','ほ',
-    'ま','み','む','め','も',
-    'や','ー','ゆ','ー','よ',
-    'ら','り','る','れ','ろ',
-    'わ','を','ん','ー','ー',
-
-    
-    'が','ぎ','ぐ','げ','ご',
-    'ざ','じ','ず','ぜ','ぞ',
-    'だ','ぢ','づ','で','ど',
-    'ば','び','ぶ','べ','ぼ',
-    'ぱ','ぴ','ぷ','ぺ','ぽ',
-    'ゃ','ゅ','ょ','っ','ー',
-];
-
-const suji = [
-    '1','2','3','4','5','6','7','8','9','0'
-];
-
-var hiraganaBut;
-function makeHiraganaBut(){
-    var row;
-    var butGroup;
-    for(var j=0; j<hiragana.length; j++){
-        if (j == 0 ||  j == 50){
-            row = document.createElement('div');
-            row.className='row g-2';
-            document.getElementById('buttonlist').appendChild(row);
-        }
-
-        if (j % 5 == 0) {
-            butGroup = document.createElement('div');
-            if (j==0){
-                butGroup.className = 'btn-group-vertical col-1 offset-1 gy-4';
-            }else if(j == 50){
-                butGroup.className = 'btn-group-vertical col-1 offset-2 gy-4';
-            }else if(j == 65){
-                butGroup.className = 'btn-group-vertical col-1 offset-1 gy-4';
-            }else if(j == 75){
-                butGroup.className = 'btn-group-vertical col-1 offset-1 gy-4';
-            }else{
-                butGroup.className = 'btn-group-vertical col-1 gy-4';
-            }
-            row.appendChild(butGroup);
-        }
-
-        var but = document.createElement('button');
-        but.className = 'btn btn-outline-info btn-sm';
-        but.type = 'button';
-        but.innerText = hiragana[j];
-        but.onclick = function() {
-            if (this.innerText != "ー") {
-                if (document.getElementById('answer').value == "") {
-                    document.getElementById('answer').value = this.innerText;
-                }else{
-                    document.getElementById('answer').value += this.innerText;
-                }
-            }
-        };
-        butGroup.appendChild(but);
-    }
+    // document.getElementById('kanjiChar').innerHTML = question.kanji;
 }
 
 function check(){
