6d8cd4de1f59eb9a12286bee727e2239e440528d
papaya202206 <yomitorisenyou2018@gmail.com>
2022-06-22 12:55:07 +0900

    kaihatu

--- common.js
+++ common.js
@@ -25,7 +25,7 @@ function makeHiraganaBut(obj1, obj2){
         if (j == 0 ||  j == 50){
             row = document.createElement('div');
             row.className='row g-2';
-            obj1.appendChild(row);
+            obj1.append(row);
         }
 
         if (j % 5 == 0) {
@@ -41,7 +41,7 @@ function makeHiraganaBut(obj1, obj2){
             }else{
                 butGroup.className = 'btn-group-vertical col-1 gy-4';
             }
-            row.appendChild(butGroup);
+            row.append(butGroup);
         }
 
         var but = document.createElement('button');
@@ -57,7 +57,7 @@ function makeHiraganaBut(obj1, obj2){
                 }
             }
         };
-        butGroup.appendChild(but);
+        butGroup.append(but);
     }
 }
 
@@ -65,31 +65,39 @@ const suji = [
     '1','2','3','4','5','6','7','8','9','.','0','C'
 ];
 
+function butAction(value, obj2){
+    if (value == "C") {
+        obj2.val("");
+    }else{
+        if (obj2.val() == "") {
+            obj2.val(value);
+        }else{
+            obj2.val(obj2.val() + " " + value);
+        }
+    }
+}
+
 function makeSujiBut(obj1, obj2){
     var row;
     var butGroup;
-    for(var j=0; j<suji.length; j++){
-        if (j % 3 == 0){
-            row = document.createElement('div');
-            row.className='row g-2';
-            obj1.appendChild(row);
-        }
 
-        var but = document.createElement('button');
-        but.className = 'btn btn-outline-info btn-lg';
-        but.type = 'button';
-        but.innerText = suji[j];
-        but.onclick = function() {
-            if (this.innerText == "C") {
-                obj2.value = "";
-            }else{
-                if (obj2.value == "") {
-                    obj2.value = this.innerText;
-                }else{
-                    obj2.value += this.innerText;
-                }
-            }
-        };
-        row.appendChild(but);
-    }
+    var row1 = $('<div>', { id:"row1", class:'row g-2' });
+    row1.append($('<button>', { id:"col11", class:'btn btn-outline-info btn-lg', type:"button", text:"1", onclick:butAction("1",obj2)}));
+    row1.append($('<button>', { id:"col12", class:'btn btn-outline-info btn-lg', type:"button", text:"2", onclick:butAction("2",obj2)}));
+    row1.append($('<button>', { id:"col13", class:'btn btn-outline-info btn-lg', type:"button", text:"3", onclick:butAction("3",obj2)}));
+    obj1.append(row1);
+
+    var row2 = $('<div>', { id:"row2", class:'row g-2' });
+    row2.append($('<button>', { id:"col21", class:'btn btn-outline-info btn-lg', type:"button", text:"4", onclick:butAction("4",obj2)}));
+    row2.append($('<button>', { id:"col22", class:'btn btn-outline-info btn-lg', type:"button", text:"5", onclick:butAction("5",obj2)}));
+    row2.append($('<button>', { id:"col23", class:'btn btn-outline-info btn-lg', type:"button", text:"6", onclick:butAction("6",obj2)}));
+    obj1.append(row2);
+
+    var row3 = $('<div>', { id:"row3", class:'row g-2' });
+    row3.append($('<button>', { id:"col31", class:'btn btn-outline-info btn-lg', type:"button", text:".", onclick:butAction(".",obj2)}));
+    row3.append($('<button>', { id:"col32", class:'btn btn-outline-info btn-lg', type:"button", text:"0", onclick:butAction("0",obj2)}));
+    row3.append($('<button>', { id:"col33", class:'btn btn-outline-info btn-lg', type:"button", text:"C", onclick:butAction("C",obj2)}));
+    obj1.append(row3);
+    
+    
 }
